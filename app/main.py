75c466c08063fea96f8177757aa7573fd0fe22a5
anar <anarnurayev32@gmail.com>
2026-09-25 20:16:08 +0400

    requirements.txt added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, status
 from .routers import public, admin
+
 
 app = FastAPI()
 
 app.include_router(public.router)
 app.include_router(admin.router)
+
+# === CORSMiddleware ===
+
+# origins = ["*"]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=False,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 # ---ROOT URL---
